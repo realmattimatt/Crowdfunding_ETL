e245fc45cccb33346e45c0253137d4a8b2c6a32b
@mattimatt-wMdRJi <128356316+realmattimatt@users.noreply.github.com>
2025-01-15 01:04:35 -0600

    Added more explicit notes on what order to run the db file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,26 +1,26 @@
-DROP TABLE IF EXISTS campaign;
-DROP TABLE IF EXISTS subcategory;
-DROP TABLE IF EXISTS category;
-DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS campaign; -- run 1st
+DROP TABLE IF EXISTS subcategory; -- run 2nd
+DROP TABLE IF EXISTS category; -- run 3rd
+DROP TABLE IF EXISTS contacts; -- run 4th
 
 CREATE TABLE contacts (
     contact_id VARCHAR(10) PRIMARY KEY NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
-);
+); -- highlight this entire CREATE TABLE and run 5th
 
 CREATE TABLE category (
     category_id VARCHAR(10) PRIMARY KEY NOT NULL,
     category VARCHAR(50) NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE
-);
+); -- highlight this entire CREATE TABLE and run 6th
 
 CREATE TABLE subcategory (
     subcategory_id VARCHAR(10) PRIMARY KEY NOT NULL,
     subcategory VARCHAR(50) NOT NULL,
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id) ON DELETE CASCADE
-);
+); -- highlight this entire CREATE TABLE and run 7th
 
 CREATE TABLE campaign (
     cf_id INT PRIMARY KEY NOT NULL,
@@ -40,9 +40,12 @@ CREATE TABLE campaign (
     FOREIGN KEY (contact_id) REFERENCES contacts(contact_id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE CASCADE,
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id) ON DELETE CASCADE
-);
+); -- highlight this entire CREATE TABLE and run 8th
+
+
 -- Uncomment as needed below to test. 
 -- SELECT * FROM contacts;
 -- SELECT * FROM category;
 -- SELECT * FROM subcategory;
 -- SELECT * FROM campaign;
+-- See README.md for the order to import
